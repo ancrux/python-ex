e6f39981992cfaa515062dbd12ec557cf45fb667
Angus Liu (RD-TW) <angus_liu@trend.com.tw>
2015-08-27 17:57:44 +0800

    [update] dynamic import

--- dynamic_import/import.py
+++ dynamic_import/import.py
@@ -1,6 +1,11 @@
 
 print "imported as %s\n" % __name__
 
+import os, sys
+import imp
+import pkgutil # since python 2.3
+# import importlib # since python 2.7
+
 import_entries = {
     'g_time': {'from': 'time', 'import': 'time', 'as': 'g_time'}, # from time import time as g_time
     'g_gmtime': {'from': 'time', 'import': 'gmtime', 'as': 'g_gmtime'}, # from time import gmtime as g_gmtime
@@ -38,3 +43,28 @@ if __name__ == '__main__':
 
     #print dir(list)
     #print dir(dict)
+    
+    # pkgutil: check if module installed
+    pkg = pkgutil.find_loader('time.zzz')
+    print pkg
+    
+    # imp: check if module installed
+    try:
+        imp.find_module('time.zzz')
+        found = True
+    except ImportError:
+        found = False
+    finally:
+        print found
+    
+    # pkgutils.itermodules(): list if module installed
+    for loader, name, ispkg in pkgutil.iter_modules():
+        #print (loader, name, ispkg)
+        pass
+        
+    # sys.modules: list if module is imported/loaded
+    print sys.path # list python lookup path
+    for key, val in sys.modules.iteritems():
+        #print (key, val)
+        pass
+    
