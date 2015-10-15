fe7f71ce0aaa897a62f656f1b4809a9e368b0d95
Angus Liu (RD-TW) <angus_liu@trend.com.tw>
2015-10-15 16:42:06 +0800

    [add] comments for MANIFEST.in

--- setuptools/setup.py
+++ setuptools/setup.py
@@ -10,11 +10,19 @@ class my_install(install):
         install.run(self)
         print '=== post-install ==='
 
+'''
+In order to use MANIFEST.in to include/exclude files,
+we must put the folder of include/exclude files to find_packages(exclude=['somefolder']) list (so these files are considered data files)
+any files not in the exclude list are considered source code and cannot be controlled by MANIFEST.in
+
+about find_packages(exclude=['somefolder']):
+exclude=['app.web.*'] will remove subfolders in app/web/ but not web/app/*.py
+'''
 setup(
     name = "mylib", # required
     version = 0.1, # required
-    packages = find_packages(), # required
-    #include_package_data = True, # if use MANIFEST.in
+    packages = find_packages(exclude=['tests']), # required
+    #include_package_data = True, # if wish to manage data files thru MANIFEST.in, set include_package_data = True
     cmdclass={'install': my_install},
 )
 
