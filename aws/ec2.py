19edb0d2f91bdda04cc362918a5f2a383a1be956
Angus Liu (RD-TW) <angus_liu@trend.com.tw>
2016-01-13 16:36:51 +0800

    [update] ec2 example

--- aws/ec2.py
+++ aws/ec2.py
@@ -8,13 +8,23 @@ https://boto3.readthedocs.org/en/latest/index.html
 - disable it when not used (e.g after deployment) for maximum security  
 """
 import boto3
+from pprint import pprint
+
 
 ec2 = boto3.resource('ec2')
 
 # show all EC2 instances
 instances = ec2.instances.all()
 for instance in instances:
-    print(instance.id, instance.instance_type)
+    # Ref: https://boto3.readthedocs.org/en/latest/reference/services/ec2.html#instance
+    # Note: instance name is stored in tags(list) with key: 'Name'
+    """
+    # list all instance attributes
+    print "=========="
+    for attr in (attr for attr in dir(instance) if not attr.startswith('__')):
+        print "%s = %s" % (attr, getattr(instance, attr))
+    """
+    print(instance.id, instance.instance_type, instance.tags)
     
 
 
